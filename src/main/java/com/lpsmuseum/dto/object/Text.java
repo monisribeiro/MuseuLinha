be6014da79a000ff24e0ee6ced5e967d4eed41d3
monisribeiro <monisribeiro@gmail.com>
2015-10-09 17:17:36 -0300

    initial

---
+++ src/main/java/com/lpsmuseum/dto/object/Text.java
@@ -0,0 +1,28 @@
+package com.lpsmuseum.dto.object;
+
+import com.lpsmuseum.dto.MuseologicalObject;
+import com.lpsmuseum.entity.MuseologicalObjectDO;
+import com.lpsmuseum.entity.TextDO;
+
+public class Text extends MuseologicalObject {
+        private String text;
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+	@Override
+	public MuseologicalObjectDO getEntity(){
+		TextDO objDO = new TextDO();
+		if(getId() != null) objDO.setId(getId());
+		objDO.setName(getName());
+		objDO.setDate(getDate().getTime());
+		objDO.setObjectType(getObjectType());
+		objDO.setText(getText());
+		return objDO;
+	}
+}
